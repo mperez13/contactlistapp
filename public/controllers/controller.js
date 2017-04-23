7d947abfb2a016f87da7f48936b0e8c0fca9f5fc
Maria Perez <mfperez13@gmail.com>
2017-04-22 19:12:17 -0700

    move dummy data to server

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -3,24 +3,5 @@ const myApp = angular.module('todolistApp', []);
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http){
   console.log("Hello World from controller");
 
-  $http.get('/contactlist')
-
-  person1 = {
-    name: 'Tim',
-    email: 'tim@email.com',
-    number: '(111) 111-1111'
-  };
-  person2 = {
-    name: 'Jane',
-    email: 'jane@email.com',
-    number: '(222) 222-2222'
-  };
-  person2 = {
-    name: 'John',
-    email: 'john@email.com',
-    number: '(333) 333-3333'
-  };
-
-  const contactlist = [person1, person2, person3];
-  $scope.contactlist = contactlist;
+  $http.get('/contactlist');
 }]);
--- server.js
+++ server.js
@@ -6,6 +6,24 @@ app.use(express.static(__dirname + "/public"));
 
 app.get('/contactlist',function(req, res){
   console.log("I received a GET request.");
+
+  person1 = {
+    name: 'Tim',
+    email: 'tim@email.com',
+    number: '(111) 111-1111'
+  };
+  person2 = {
+    name: 'Jane',
+    email: 'jane@email.com',
+    number: '(222) 222-2222'
+  };
+  person3 = {
+    name: 'John',
+    email: 'john@email.com',
+    number: '(333) 333-3333'
+  };
+
+  let contactlist = [person1, person2, person3];
 });
 
 app.listen(3000);
