f370b36f4ac08f46ab3aa0a4da0f264b876658ed
Maria Perez <mfperez13@gmail.com>
2017-04-22 20:02:39 -0700

    have server find data from database

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ app.use(express.static(__dirname + "/public"));
 app.get('/contactlist',function(req, res){
   console.log("I received a GET request.");
 
+  db.contactlist.find(function(err,docs){
+    console.log(docs); // test to make sure we received the data
+    res.json(docs); // sends data back to the controller
+  });
 });
 
 app.listen(3000);
