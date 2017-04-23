e0e2633f2f2f741271da05952a3126d7987e42ef
Maria Perez <mfperez13@gmail.com>
2017-04-22 20:14:45 -0700

    have server listen for this post

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -8,4 +8,13 @@ myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http){
     console.log("I got the data requested.");
     $scope.contactlist = response.data;
   });
+
+  $scope.addContact = function(){
+    console.log($scope.contact); // checks that we received data from the input boxes
+    // $scope.contact is the data sent to the server
+    $http.post('/contactlist', $scope.contact).then(function(response){
+      console.log(response);
+    });
+  };
+
 }]);
--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
 const mongojs = require('mongojs');
+const bodyParser = require('body-parser');
 // tells  you what database and collections will be used here
 const db = mongojs('contactlist', ['contactlist']);
 
 // static tells server where to look for our static files (HTML, CSS, JS, images)
 app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
 
 app.get('/contactlist',function(req, res){
   console.log("I received a GET request.");
@@ -13,6 +15,16 @@ app.get('/contactlist',function(req, res){
   db.contactlist.find(function(err,docs){
     console.log(docs); // test to make sure we received the data
     res.json(docs); // sends data back to the controller
+  });
+});
+
+// have our server listen
+app.post('/contactlist', function(req, res){
+  console.log(req.body); // print data recieves; won't work because the server doesn't know how to parse it
+                        // to parse the body need to use the module: body-parser
+  // inserts the data into the database & sends data from the database back to the controller
+  db.contactlist.insert(req.body, function(err, doc){
+    res.json(doc);
   });
 });
 
