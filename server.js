01b186f2129c1e2e78caf705d6c7f284cfad2aec
Maria Perez <mfperez13@gmail.com>
2017-04-22 21:01:29 -0700

    put in server

--- server.js
+++ server.js
@@ -36,13 +36,17 @@ app.delete('/contactlist/:id', function(req, res){
   });
 });
 
-
 app.get('/contactlist/:id', function(req,res){
   var id = req.params.id;
   console.log(id);
   db.contactlist.findOne({_id: mongojs.ObjectId(id)}, function(err, doc){
       res.json(doc);
   });
+});
+
+app.put('/contactlist/:id', function(req, res){
+    var id = req.params.id;
+    console.log(req.body.name);
 });
 
 app.listen(3000);
