105f421f008d19ab6e5b3438d0a2847d363033ac
Maria Perez <mfperez13@gmail.com>
2017-04-22 20:00:44 -0700

    delete dummy data

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const mongojs = require('mongojs');
-
 // tells  you what database and collections will be used here
 const db = mongojs('contactlist', ['contactlist']);
 
@@ -10,23 +9,7 @@ app.use(express.static(__dirname + "/public"));
 
 app.get('/contactlist',function(req, res){
   console.log("I received a GET request.");
-  person1 = {
-    name: 'Tim',
-    email: 'tim@email.com',
-    number: '(111) 111-1111'
-  };
-  person2 = {
-    name: 'Jane',
-    email: 'jane@email.com',
-    number: '(222) 222-2222'
-  };
-  person3 = {
-    name: 'John',
-    email: 'john@email.com',
-    number: '(333) 333-3333'
-  };
-  let contactlist = [person1, person2, person3];
-  res.json(contactlist);
+
 });
 
 app.listen(3000);
